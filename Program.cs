31f16a85256c8658c35aba154fe4cb7b6bca7e76
erikappelgren123@gmail.com <erikappelgren123@gmail.com>
2020-10-13 15:45:39 +0200

    test2 lol

--- Program.cs
+++ Program.cs
@@ -9,14 +9,34 @@ namespace progExercise_01
             string name = "Erik Appelgren";
             string street = "Kunskapslänken 88";
             string zipcode = "583 28";
+            string topInput;
+            int topNumber;
+            string leftInput;
+            int leftNumber;
+                Console.Write("Hur långt från toppen: ");
+                    topInput = Console.ReadLine();
+                    topNumber = Convert.ToInt32(topInput);
+            
+
+                Console.Write("Hur långt från vänster: ");
+                    leftInput = Console.ReadLine();
+                    leftNumber = Convert.ToInt32(leftInput);
+
+            
 
             Console.BackgroundColor = ConsoleColor.DarkCyan;
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Clear();
-            
-                Console.WriteLine(name);
-                Console.WriteLine(street);
-                Console.WriteLine(zipcode);
+
+
+            Console.SetCursorPosition(leftNumber, topNumber);
+            Console.WriteLine(name);
+            Console.SetCursorPosition(leftNumber, topNumber + 1);
+            Console.WriteLine(street);
+            Console.SetCursorPosition(leftNumber, topNumber + 2);
+            Console.WriteLine(zipcode);
+          
+
             
             Console.ReadKey();
         }
