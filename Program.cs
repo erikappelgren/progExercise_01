f620f69e5ee8143ae3fe53df9f28415ef927dd0d
erikappelgren123@gmail.com <erikappelgren123@gmail.com>
2020-10-13 14:53:34 +0200

    test lol

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace progExercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Erik Appelgren";
+            string street = "Kunskapslänken 88";
+            string zipcode = "583 28";
+
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            
+                Console.WriteLine(name);
+                Console.WriteLine(street);
+                Console.WriteLine(zipcode);
+            
+            Console.ReadKey();
+        }
+    }
+}
